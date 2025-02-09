092c9d3d166351a8b3740403b885ce415e8ef688
pooja <129350191+pooja838@users.noreply.github.com>
2025-02-09 21:44:30 +0530

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -8,17 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
-public class StartApplication {
+public class Application {
 
     @GetMapping("/")
-    public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+    public String home(final Model model) {
+        model.addAttribute("pageTitle", "Spring Boot Application with Maven");
+        model.addAttribute("message", "This application is successfully deployed on Kubernetes using Argo CD!");
         return "index";
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
